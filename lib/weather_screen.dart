db0e4e1246d519a2c5b51a2f4ee8990b001d4b8d
shimon <shimonbhuiya02@gmail.com>
2026-01-13 02:21:33 +0600

    add variable

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -9,17 +9,43 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
+
+  String city = "";
+  String temperature = "";
+  String feelsLike = "";
+  String windSpeed = "";
+  String humidity = "";
+  String visibility = "";
+  String pressure = "";
+  String uvIndex = "";
+  String description = "";
+  String icon = "";
+  String localTime = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(title: Text("Weather App"),),
-      body: Padding(padding: EdgeInsets.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+      appBar: AppBar(title: Text("Weather App")),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter City Name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: Text("Get Weather")),
+            SizedBox(height: 10),
+            Image.network(
+              "https://npr.brightspotcdn.com/dims3/default/strip/false/crop/2000x1304+0+0/resize/1100/quality/50/format/jpeg/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Fd1%2F41%2F90fa03af4636a1f5bc89474d4c2a%2Fap24324862737556.jpg",
+            ),
 
-        ],
-      ),
+          ],
+        ),
       ),
     );
   }
