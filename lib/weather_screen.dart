84a4807fb4f0600ee8544847f2028842d5a1b9da
shimon <shimonbhuiya02@gmail.com>
2026-01-25 03:05:31 +0600

    add variable

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
@@ -9,6 +10,7 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
+  TextEditingController cityController = TextEditingController();
 
   String city = "";
   String temperature = "";
@@ -22,6 +24,14 @@ class _WeatherScreenState extends State<WeatherScreen> {
   String icon = "";
   String localTime = "";
 
+  final String apiKey = "291e2d051e624ed488974910240410";
+  final String baseUrl = "http://api.weatherapi.com/v1/current.json";
+
+  Future fetchWeather() async {
+    final url = "$baseUrl?key=$apiKey&q$city";
+    final response = await http.get(url);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,6 +42,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
+              controller: cityController,
               decoration: InputDecoration(
                 hintText: "Enter City Name",
                 border: OutlineInputBorder(),
@@ -40,10 +51,55 @@ class _WeatherScreenState extends State<WeatherScreen> {
             SizedBox(height: 20),
             ElevatedButton(onPressed: () {}, child: Text("Get Weather")),
             SizedBox(height: 10),
+
+            Text(
+              temperature,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              feelsLike,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              feelsLike,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              windSpeed,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+
+            Text(
+              visibility,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              pressure,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              uvIndex,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              localTime,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              description,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
             Image.network(
               "https://npr.brightspotcdn.com/dims3/default/strip/false/crop/2000x1304+0+0/resize/1100/quality/50/format/jpeg/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Fd1%2F41%2F90fa03af4636a1f5bc89474d4c2a%2Fap24324862737556.jpg",
             ),
-
           ],
         ),
       ),
